2ef691e5d7b667cde74dcae5c9432c0442f06dd7
Prateek <prateek.kumar.ind@gmail.com>
2025-07-26 16:46:25 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import AddTask from "./components/AddTask";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <h1 className="text 2xl font-bold py-4 pl-6">The Task Tracker</h1>
+      <p className="text-xl pl-6">Hi there</p>
+      <div className="flex flex-row items-center">
+        <p className="text-xl pl-6">Click</p>
+        <AddTask />
+        <p className="text-xl ">to add new task</p>
+      </div>
+    </>
+  );
 }
